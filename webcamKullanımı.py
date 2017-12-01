cdfdf2cea1e83e000af39daa206521edc66dd018
Omer Mert KAYA <l1421012018@stud.sdu.edu.tr>
2017-12-01 15:48:54 +0300

    webcamKullanımı
    Opencv yardımı ile bilgisayarin web kamerasini acmak ve görüntü almak. Q tusu ile cıkıs saglanır.

---
+++ webcamKullanımı.py
@@ -0,0 +1,15 @@
+import cv2  # opencv import edilir
+import numpy as np  # numpy import edilir
+
+cap = cv2.VideoCapture(0) #bilgisayarin web kamerasından video dondurur
+
+while True:   # bu while dongusu sayesinde ekran olusturulur
+    ret, frame = cap.read() 
+
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):  #ekranda q tusuna basıldığında sona erdirir.
+        break
+
+cap.release()
+cv2.destroyAllWindows()
